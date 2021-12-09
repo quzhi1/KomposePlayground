72644349ea5857bb46cda89d299c248fe25b1542
Zhi Qu <zhi.q@nylas.com>
2021-12-08 16:55:17 -0800

    hello-go can talk to Redis via Kubernetes DNS resolution

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+
+	"github.com/go-redis/redis/v8"
 )
+
+var ctx = context.Background()
+var WELCOME_MESSAGE = "Hello word 12/08/2021\n"
 
 func main() {
 	http.HandleFunc("/", hello)
@@ -11,5 +17,21 @@ func main() {
 }
 
 func hello(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintf(w, "Hello word 12/07/2021 changed\n")
+	opts := redis.Options{
+		Addr:     "redis-master.default.svc.cluster.local:6379",
+		Password: "",
+		DB:       0,
+	}
+	redisClient := redis.NewClient(&opts)
+	err := redisClient.Set(ctx, "key", WELCOME_MESSAGE, 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := redisClient.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprint(w, val)
 }
